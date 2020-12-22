62760d41512fa6e6c6d4a9a593fa421d64248b8a
chensongtao <chensongtao@9fbank.com.cn>
2020-12-22 16:18:13 +0800

    打包文件

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,12 @@ function resolvePath(dir) {
 }
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? process.env.VUE_PUBLIC_PATH : process.env.VUE_APP_BASE_URL,
+    // publicPath: process.env.NODE_ENV === 'production' ? process.env.VUE_PUBLIC_PATH : process.env.VUE_APP_BASE_URL,
+    publicPath: './',
     assetsDir: "static",
     productionSourceMap: false,     // 不需要生产环境的 source map，false 以加速生产环境构建
-    outputDir: process.env.OUT_PUT_DIR,
+    // outputDir: process.env.OUT_PUT_DIR,
+    outputDir: 'docs',  // 部署到 github
 
     lintOnSave: false,  // 保存时不使用 eslint 
     configureWebpack: (config) => {
